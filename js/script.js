9c7ddf2f678a416d1846e45a091776b25072aaa9
Lorencini <arthnilo@gmail.com>
2023-03-14 12:06:49 -0300

    perguntas-JS-pronto

--- js/script.js
+++ js/script.js
@@ -28,4 +28,20 @@ parametros.forEach(ativarProduto);
 
 const perguntas = document.querySelectorAll(".perguntas button");
 
-console.log(perguntas)
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta)
+};
+
+perguntas.forEach(eventosPerguntas)
+
